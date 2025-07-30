65ccfd9d6336eb9eb05f5de74f0a39103e1e6fbe
wanzbrayyy <wanzbray@gmail.com>
2025-07-30 18:08:24 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  botToken: "7330307947:AAEey_Re6CBEi3kUKds1CxU6vkWUq_CxrCc",
+  adminId: "7774371395",
+  mongodbUri: "mongodb+srv://zanssxploit:pISqUYgJJDfnLW9b@cluster0.fgram.mongodb.net/scmarket_db?retryWrites=true&w=majority",
+  ownerName: "wanzofc",
+  ownerUsername: "maverick_dark",
+  botUsername: "wanzofc_shop_bot",
+  botDescription: "halo selamat datang di Wanz Store! Silakan lihat-lihat produk kami.",
+  groupLink: "https://t.me/wanzstore",
+  qrisImagePath: "images/qris.jpg",
+  midtransServerKey: "Mid-server-zvgGUiY7SS-HS_qhWLkqZQuL",
+  midtransClientKey: "Mid-client-IoIOg2RqJNZgKpY6",
+  minimumDeposit: 10000,
+  botBaseUrl: "https://bott-production-2188.up.railway.app",
+  apiId: 25054644,
+  apiHash: "d9c07f75d488f15cb655049af0fb686a",
+  paymentLink: "https://saweria.co/wanzofc",
+  channelId: "@menfess002" 
+};
