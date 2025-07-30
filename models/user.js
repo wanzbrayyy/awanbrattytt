8f1c6819417bfa7e58d7832cc8664b14a540f27d
wanzbrayyy <wanzbray@gmail.com>
2025-07-31 03:19:57 +0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     chatId: { type: Number, required: true, unique: true },
+    username: { type: String, required: false },
     email: { type: String, unique: false, sparse: true },
     saldo: { type: Number, default: 0 },
     daftar: { type: Boolean, default: false },
+    isPremium: { type: Boolean, default: false },
     type: { type: String, enum: ['private', 'group', 'supergroup', 'channel'], default: 'private' },
     joinDate: { type: Date, default: Date.now },
     location: {
